44175161ebc0bd58bfec19a8a9fd50627e87af72
lucascencig <cenciglucas@gmail.com>
2024-03-05 19:59:50 -0300

    newsletter section components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 
-const Button = ({ label, iconUrl, backgroundColor, textColor, borderColor }) => {
+const Button = ({ label, iconUrl, backgroundColor, textColor, borderColor, fullWidth }) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
       ${backgroundColor
           ? `${backgroundColor} ${textColor} ${borderColor} hover:bg-red-400`
           : "bg-coral-red text-white border-coral-red"
-        } rounded-full "}`}
-    // ${fullWidth && "w-full
+        } rounded-full ${fullWidth && "w-full"}`}
+
     >
 
       {label}
